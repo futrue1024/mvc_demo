0fca536268838fa36e2330c051f1fa7d5001ae59
陈祥 <indexchen@foxmail.com>
2020-07-23 22:55:02 +0800

    mvc commit

--- src/app1.js
+++ src/app1.js
@@ -1,10 +1,25 @@
 import "./app1.css";
 import $ from "jquery";
-const html = `
-<section id="app1">
+
+const eventBus = $(window); //设定事件，监听触发事件。
+//数据相关放到m
+const m = {
+  data: {
+    n: parseInt(localStorage.getItem("n")),
+  },
+  update(data) {
+    Object.assign(m.data, data);
+    eventBus.trigger("m:updated"); //只要m更新了就触发指定事件
+  },
+};
+//视图相关放到v
+const v = {
+  container: null,
+  html: `
+<div>
 <div id="appContent1">
   <div class="output">
-    <span id="number">100</span>
+    <span id="number">{{n}}</span>
   </div>
   <div class="actions">
     <button id="add">+1</button>
@@ -13,43 +28,61 @@ const html = `
     <button id="divide">÷2</button>
   </div>
 </div>
-</section>
-`;
+</div>
+`,
+  init(container) {
+    v.container = $(container);
+  },
+  render(n) {
+    if (v.container.children.length === 0) {
+      $(v.html.replace("{{n}}", n)).prependTo($(v.container));
+    } else {
+      v.container.empty();
+      $(v.html.replace("{{n}}", n)).prependTo($(v.container));
+    }
+  },
+};
+//其他放到c
+const c = {
+  init(container) {
+    v.init(container);
+    v.render(m.data.n);
+    c.autoEvent();
+    eventBus.on("m:updated", () => {
+      v.render(m.data.n); //一旦m.data更新，就将n重新渲染进页面
+      localStorage.setItem("n", m.data.n);
+    });
+  },
+  event: {
+    "click #add": "add",
+    "click #minus": "minus",
+    "click #divide": "divide",
+    "click #multiply": "multiply",
+  },
+  add() {
+    m.update({ n: m.data.n + 1 });
+  },
+  minus() {
+    m.update({ n: m.data.n - 1 });
+  },
+  multiply() {
+    m.update({ n: m.data.n * 2 });
+  },
+  divide() {
+    m.update({ n: m.data.n / 2 });
+  },
+  autoEvent() {
+    for (let key in c.event) {
+      console.log(key);
+      let value = c[c.event[key]];
+      let spaceIndex = key.indexOf(" ");
+      let part1 = key.slice(0, spaceIndex);
+      let part2 = key.slice(spaceIndex + 1);
+      v.container.on(part1, part2, value);
+    }
+  },
+};
 
-const $element = $(html).prependTo($("body > .page"));
-//$代表选择某一标签。
-const $add = $("#add");
-const $minus = $("#minus");
-const $multiply = $("#multiply");
-const $divide = $("#divide");
-const $number = $("#number");
-let n = localStorage.getItem("n");
-$number.text(n || 100);
+//初始化html
 
-$divide.on("click", () => {
-  let n = parseInt($number.text());
-  n /= 2;
-  localStorage.setItem("n", n);
-  $number.text(n);
-});
-
-$add.on("click", () => {
-  let n = parseInt($number.text());
-  n += 1;
-  localStorage.setItem("n", n);
-  //将每次操作存入本地，使得刷新页面数值不丢失。
-  //将每次操作的键值名都设为n，使得localStorage每次都是最新的n值。
-  $number.text(n);
-});
-$minus.on("click", () => {
-  let n = parseInt($number.text());
-  n -= 1;
-  localStorage.setItem("n", n);
-  $number.text(n);
-});
-$multiply.on("click", () => {
-  let n = parseInt($number.text());
-  n *= 2;
-  localStorage.setItem("n", n);
-  $number.text(n);
-});
+export default c;
--- src/app2.js
+++ src/app2.js
@@ -1,30 +1,80 @@
 import "./app2.css";
 import $ from "jquery";
-const html = ` <section id="app2">
-<ol id="tab-bar">
-  <li>1111</li>
-  <li>2222</li>
-</ol>
-<ol id="content">
-  <li>选择1111</li>
-  <li>选择2222</li>
-</ol>
-</section>`;
-const $element = $(html).appendTo($("body>.page"));
-const $tabBar = $("#tab-bar");
-const $content = $("#content");
-const index = localStorage.getItem("index");
 
-$tabBar.on("click", "li", (e) => {
-  const $li = $(e.currentTarget);
-  $li.addClass("active").siblings().removeClass("active");
-  const index = $li.index();
-  localStorage.setItem("index", index);
-  $content
-    .children()
-    .eq(index)
-    .addClass("active")
-    .siblings()
-    .removeClass("active");
-});
-$tabBar.children().eq(index).trigger("click");
+const eventBus = $(window); //设定事件，监听触发事件。
+//数据相关放到m
+const m = {
+  data: {
+    index: parseInt(localStorage.getItem("index")) || 0,
+  },
+  update(index) {
+    Object.assign(m.data, index);
+    eventBus.trigger("m:updated"); //只要m更新了就触发指定事件
+  },
+};
+
+const v = {
+  container: null,
+  html: (index) => {
+    return `
+  <div>
+  <ol id="tab-bar">
+  <li class="${index === 0 ? "active" : null}" data-index='0'>1111</li>
+  <li class="${index === 1 ? "active" : null}" data-index='1'>2222</li>
+  </ol>
+  <ol id="content">
+    <li class="${index === 0 ? "active" : null}" >选择1111</li>
+    <li class="${index === 1 ? "active" : null}">选择2222</li>
+  </ol>
+  </div>
+`;
+  },
+  init(container) {
+    v.container = $(container);
+  },
+  render(index) {
+    if (v.container.children.length === 0) {
+      $(v.html(index)).prependTo($(v.container));
+    } else {
+      v.container.empty();
+      $(v.html(index)).prependTo($(v.container));
+    }
+  },
+};
+
+const c = {
+  init(container) {
+    v.init(container);
+    v.render(m.data.index); //初识值
+    c.autoEvent();
+    eventBus.on("m:updated", () => {
+      v.render(m.data.index); //一旦m.data更新，就将n重新渲染进页面
+      localStorage.setItem("n", m.data.index);
+    });
+  },
+  event: {
+    "click #tab-bar li": "x",
+  },
+  x(e) {
+    console.log(e.currentTarget);
+    let liIndex = parseInt(e.currentTarget.dataset.index);
+    console.log(liIndex);
+    m.update({ index: liIndex });
+  },
+
+  autoEvent() {
+    for (let key in c.event) {
+      console.log(key);
+      let value = c[c.event[key]];
+      let spaceIndex = key.indexOf(" ");
+      let part1 = key.slice(0, spaceIndex);
+      console.log(part1);
+      let part2 = key.slice(spaceIndex + 1);
+      console.log(part2);
+      v.container.on(part1, part2, value);
+      console.log(1);
+    }
+  },
+};
+
+export default c;
